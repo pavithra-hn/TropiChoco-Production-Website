c3a206e16c127fa4371d84c84dcc37dd21193b19
pavithra-hn <pavithrahn56@gmail.com>
2026-02-25 09:53:22 +0530

    update basePath for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
     // basePath automatically handles asset paths too, so assetPrefix is not needed
-    basePath: isProd ? '/Products.com' : '',
+    basePath: isProd ? '/TropiChoco-Production-Website' : '',
     images: {
         unoptimized: true,
     },
